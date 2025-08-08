b2811bf2f06928bd3c180ea21c6c83dcf8a87428
RewajNeupane <neupanerewaj123@gmail.com>
2025-08-08 09:23:46 +0545

    host

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -88,20 +88,20 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES ={
-# #     'default': dj_database_url.config(
-# #         default=os.environ.get('DATABASE_URL')
-# #     )
-# # }
-
-
-
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
+DATABASES ={
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 
 # Password validation
